d9ddc36d6054b4926c9d646e4c02ddaa6eeb1a2b
RK Sanayaima Singh <rksanayai@gmail.com>
2023-09-17 22:58:24 +0530

    Completed Rustlings as of 17th Sept 2023, some advanced topics need more readings though

---
+++ clippy/target/debug/deps/clippy3-600385bf457f61a5.d
@@ -0,0 +1,8 @@
+/Users/rksys/workspace/rust_dev/rustling/rustlings/exercises/clippy/target/debug/deps/clippy3-600385bf457f61a5.rmeta: clippy3.rs Cargo.toml
+
+/Users/rksys/workspace/rust_dev/rustling/rustlings/exercises/clippy/target/debug/deps/clippy3-600385bf457f61a5.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
