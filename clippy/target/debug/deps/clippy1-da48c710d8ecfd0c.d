05def5c1e9d055e7d0beeacd713c7195cfea184f
RK Sanayaima Singh <rksanayai@gmail.com>
2023-09-17 21:14:24 +0530

    macro DONE, but need to study more on thread, macro, and smart pointer

---
+++ clippy/target/debug/deps/clippy1-da48c710d8ecfd0c.d
@@ -0,0 +1,8 @@
+/Users/rksys/workspace/rust_dev/rustling/rustlings/exercises/clippy/target/debug/deps/clippy1-da48c710d8ecfd0c.rmeta: clippy1.rs Cargo.toml
+
+/Users/rksys/workspace/rust_dev/rustling/rustlings/exercises/clippy/target/debug/deps/clippy1-da48c710d8ecfd0c.d: clippy1.rs Cargo.toml
+
+clippy1.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
